2e11f37d9d073bcf8920d81703553a7a66cb589e
caesar510 <57384388+caesar510@users.noreply.github.com>
2021-11-30 08:00:04 +0800

    energy

--- Assets/mainchar_movement.cs
+++ Assets/mainchar_movement.cs
@@ -144,7 +144,6 @@ public class mainchar_movement : MonoBehaviour
                             }
                             
                         }
-                        energy-= Time.deltaTime * 10f;
                     }
             }
             if(!powerIsBroken[1])
